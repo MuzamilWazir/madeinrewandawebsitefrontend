b6666fbc37b3db19b7db2e1cd6b723df4b82857c
Wazir Muhammad Muzammil <152263083+MuzamilWazir@users.noreply.github.com>
2026-01-23 12:32:30 +0500

    heading

---
+++ src/components/Heading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Heading = ({ heading, subHeading }) => {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <h2 className="text-5xl font-bold text-primary">{heading}</h2>
+
+      {subHeading && <p className="my-5 mx-24 ">{subHeading}</p>}
+    </div>
+  );
+};
+
+export default Heading;
