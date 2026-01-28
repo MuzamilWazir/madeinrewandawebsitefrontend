1b10d9d522d8e0ed7dc3291138b87b52183f8e3c
MuzamilWazir <wazirmuhammadmuzammil@gmail.com>
2026-01-28 12:23:05 +0500

    Prduct image added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { ProductData } from "../../lib/ProductData";
 import Image from "next/image";
+import Link from "next/link";
 function Product() {
   return (
-    <div className="grid grid-cols-4 gap-3">
+    <div className="grid grid-cols-4 gap-3 my-5">
       {ProductData.map((data, index) => (
         <div
           key={index}
@@ -15,16 +16,20 @@ function Product() {
               alt="Product Image"
               width={100}
               height={100}
-              className=" w-65 h-40  border border-primary rounded-2xl "
+              className=" w-65 h-40   border border-primary rounded-2xl "
             />
           </div>
           <h2 className="font-semibold mx-8 text-xl my-1">
             {data.product_name}
           </h2>
           <p>{data.company_name}</p>
-          <button className="bg-primary p-2 w-full rounded-2xl text-sm mx-2 text-white">
+
+          <Link href={`/product/${data.slug}`} className="bg-primary p-2 w-full rounded-2xl text-sm mx-2 text-white">
+          <button >
             View More
           </button>
+          </Link>
+          
         </div>
       ))}
     </div>
